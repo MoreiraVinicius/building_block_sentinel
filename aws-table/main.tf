f54e2aaba346d66612de8f65f3f5a44c937a458e
Vinicius Moreira <moreiravinicius.job@gmail.com>
2024-10-29 09:28:09 -0300

    dynamo

--- aws-table/main.tf
+++ aws-table/main.tf
@@ -2,59 +2,116 @@ module "sentinel_datadog" {
   source = "./modules/datadog"
 }
 
-# resource "aws_sqs_queue" "sentinel_fila_principal" {
-#   # Nome da fila
-#   name = "sentinel-fila-principal"
-#   # Tempo de espera da mensagem (segundos)
-#   delay_seconds = 0
-#   # Tempo de vida da mensagem (segundos)
-#   message_retention_seconds = 86400
-#   # Política de recebimento de mensagens
-#   receive_wait_time_seconds = 10
+provider "aws" {  
+  region = "us-east-1"  
+}
 
-#   # Configuração de redrive policy para fila de mensagens mortas
-#   redrive_policy = jsonencode({
-#     deadLetterTargetArn = aws_sqs_queue.terraform_queue_deadletter.arn
-#     maxReceiveCount     = 4
-#   })
+resource "aws_dynamodb_table" "example" {  
+  name           = "example-table"  
+  hash_key       = "id_jornada"  
+  range_key      = "id_transacao"
 
-#   # Tags para identificação da fila
-#   tags = {
-#     Name        = "sentinel-fila-principal"
-#     Project = "sentinel"
-#   }
-# }
+  ttl {  
+    attribute_name = "TimeToLive"  # Attribute that stores TTL timestamps.  
+    enabled        = true  
+  }
 
-# resource "aws_sqs_queue" "sentinel_fila_dlq" {
-#   name = "sentinel-fila-dlq"
-# }
+  attribute {  
+    name = "id_jornada"  
+    type = "S"  
+  }  
 
-# resource "aws_dynamodb_table" "sentinel_dynamodb_table" {
-#   name           = "sentinel-table"
-#   hash_key       = "id"
-#   billing_mode   = "PAY_PER_REQUEST" # PROVISIONED | PAY_PER_REQUEST
-#   stream_enabled = true
-#   #A opção `stream_view_type` é usada para especificar o tipo de visualização de dados que será incluído no fluxo de dados (stream) de uma tabela do DynamoDB.  
-#   # Opções disponíveis:
-#   # 1. `NEW_IMAGE`: Isso significa que apenas as versões atualizadas dos itens serão incluídas no fluxo de dados. 
-#   # Ou seja, apenas os novos valores dos atributos serão registrados.
-#   # 2. `OLD_IMAGE`: Isso significa que apenas as versões antigas dos itens serão incluídas no fluxo de dados.
-#   # 3. `NEW_AND_OLD_IMAGES`: Isso significa que tanto as versões atualizadas quanto as antigas dos itens serão incluídas no fluxo de dados. 
-#   # 4. `KEYS_ONLY`: Isso significa que apenas as chaves dos itens serão incluídas no fluxo de dados. Nenhum valor de atributo será registrado.
-#   # Por exemplo, se você estiver interessado apenas nas alterações nos valores dos atributos, pode optar por `NEW_IMAGE` ou `OLD_IMAGE`. Se precisar de ambos, pode escolher `NEW_AND_OLD_IMAGES`. Se estiver interessado apenas nas chaves dos itens, pode escolher `KEYS_ONLY`.
-#   stream_view_type = "NEW_IMAGES" # NEW_IMAGE | OLD_IMAGE | NEW_AND_OLD_IMAGES | KEYS_ONLY
+  attribute {  
+    name = "id_transacao"  
+    type = "S"  
+  }  
 
-#   attribute {
-#     name = "id"
-#     type = "S"
-#   }
+  attribute {  
+    name = "status_nome"  
+    type = "S"  
+  }  
 
-#   ttl {
-#     attribute_name = "ttl" # Definir no codigo para 86400 que é 1 dia em segundos
-#     enabled        = true
-#   }
+  attribute {  
+    name = "datahora"  
+    type = "S"  
+  }  
 
-#   tags = {
-#     Name = "sentinel"
-#   }
-# }
+  attribute {  
+    name = "descrição"  
+    type = "S"  
+  }  
+
+  stream_enabled   = true  
+  stream_view_type = "NEW_IMAGE"  
+
+  billing_mode = "PAY_PER_REQUEST"  
+}  
+
+resource "aws_lambda_function" "dynamodb_stream_processor" {  
+  filename         = "lambda_stream_processor.zip"
+  function_name    = "dynamodb-stream-processor"  
+  role             = aws_iam_role.lambda_exec_role.arn  
+  handler          = "lambda_function.lambda_handler"  
+  runtime          = "python3.8"  
+}  
+
+resource "aws_lambda_event_source_mapping" "dynamodb_event" {  
+  event_source_arn = aws_dynamodb_table.example.stream_arn  
+  function_name    = aws_lambda_function.dynamodb_stream_processor.arn  
+  starting_position = "LATEST"  
+}  
+
+resource "aws_lambda_function" "sqs_processor" {  
+  filename         = "lambda_sqs_processor.zip" 
+  function_name    = "sqs-processor"  
+  role             = aws_iam_role.lambda_exec_role.arn  
+  handler          = "lambda_function.lambda_handler"  
+  runtime          = "python3.8"  
+}  
+
+resource "aws_lambda_event_source_mapping" "sqs_event" {  
+  event_source_arn  = aws_sqs_queue.queue.arn  
+  function_name     = aws_lambda_function.sqs_processor.arn  
+}  
+
+resource "aws_iam_role" "lambda_exec_role" {  
+  name = "lambda-exec-role"  
+
+  assume_role_policy = jsonencode({  
+    Version = "2012-10-17"  
+    Statement = [  
+      {  
+        Action = "sts:AssumeRole"  
+        Effect = "Allow"  
+        Principal = {  
+          Service = "lambda.amazonaws.com"  
+        }  
+      },  
+    ]  
+  })  
+}  
+
+# Apenas teste, substituir pelo modulo de acesso da empresa
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {  
+  role       = aws_iam_role.lambda_exec_role.name  
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"  
+}  
+
+# Apenas teste, substituir pelo modulo de acesso correspondente
+resource "aws_iam_role_policy_attachment" "dynamodb_full_access" {  
+  role       = aws_iam_role.lambda_exec_role.name  
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"  
+}  
+
+
+# Apenas teste, substituir pelo modulo de acesso correspondente
+resource "aws_iam_role_policy_attachment" "cloudwatch_put_metric_data" {  
+  role       = aws_iam_role.lambda_exec_role.name  
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchPutMetricData"  
+}  
+
+# Apenas teste, substituir pelo modulo de acesso correspondente
+resource "aws_iam_role_policy_attachment" "sqs_full_access" {  
+  role       = aws_iam_role.lambda_exec_role.name  
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"  
+}
